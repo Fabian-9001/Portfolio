4e2ea7d7fdd764ac5250ad20e6407972d3738395
Fabian Cruz Garcia <garciacruzfabian23@gmail.com>
2022-11-04 23:51:38 -0600

    Primera Version de Portafolio

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*TRANSITION*/
 const hiddenElement = document.querySelectorAll('.hidden')
 const links = document.querySelectorAll('.navbar__link')
 
@@ -10,20 +11,31 @@ const observer = new IntersectionObserver((entries) => {
             links.forEach(link => {
                 const href = link.getAttribute("href").split('#')[1]
                 const id = entry.target.id
-              
-                if(href === id) {
+
+                if (href === id) {
                     link.classList.add('link--active')
                 } else {
                     link.classList.remove('link--active')
                 }
-        })
+            })
 
-} else {
-    entry.target.classList.remove('show')
-}
+        } else {
+            entry.target.classList.remove('show')
+        }
     })
 }, {
     threshold: 0.5
 })
 
 hiddenElement.forEach(element => observer.observe(element))
+
+/*COLORS*/
+
+const navbar = document.getElementById('navbar')
+const showNavbar = document.getElementById('showNavbar')
+
+
+showNavbar.addEventListener('click', () => {
+     navbar.classList.toggle('show')
+})
+ console.log(navbar)
